27c5788bc43a8eebd0faa760e19e31bcbd890fe9
LAPTOP-JAEFD5TU\lucas <lucaswizard98@gmail.com>
2021-12-06 16:14:58 +0100

    + dice implantation

--- src/main/java/com/example/dicegame/random/JavaRandom.java
+++ src/main/java/com/example/dicegame/random/JavaRandom.java
@@ -3,8 +3,22 @@ package com.example.dicegame.random;
 import java.util.Queue;
 
 public class JavaRandom implements RandomStrategy{
+    private final int size =200;
+
+
     @Override
     public Queue<Integer> fillQueue(Queue queue) throws Exception {
-        return null;
+        final int min=1;
+        final int max=6;
+
+        for (int i=0;i<size;i++){
+            int randomDice= min+(int)(Math.random()*((max-min)+1)) ;
+            queue.add(randomDice);
+        }
+        return queue;
     }
+
+
+
+
 }
--- src/main/java/com/example/dicegame/random/RandomOrgRandom.java
+++ src/main/java/com/example/dicegame/random/RandomOrgRandom.java
@@ -5,6 +5,7 @@ import java.util.Queue;
 public class RandomOrgRandom implements RandomStrategy{
     @Override
     public Queue<Integer> fillQueue(Queue queue) throws Exception {
-        return null;
+        throw new Exception("This is not implemented");
+        //return queue;
     }
 }
