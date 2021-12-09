d19d32b12265b641d6d3a4e0a6179460752b7baf
LAPTOP-JAEFD5TU\lucas <lucaswizard98@gmail.com>
2021-12-09 16:53:06 +0100

    + fixed servlet

--- src/main/java/com/example/dicegame/servlets/LobbyServlet.java
+++ src/main/java/com/example/dicegame/servlets/LobbyServlet.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Map;
 import java.util.UUID;
 
 @WebServlet(name = "LobbyServlet", value = "/Lobby-servlet")
@@ -22,13 +24,39 @@ public class LobbyServlet extends HttpServlet {
         lobbyFunctions(request, response);
     }
 
+    private void printNames(Map<String, String[]> map) {
+        map.forEach((e, v) -> {
+            System.out.println("parameter: " + e + " has Values:");
+
+            for (String parameterValue : v) {
+                System.out.println("Value: " + parameterValue);
+            }
+
+        });
+    }
+
+    private String getParmeterValue(Map<String, String[]> map, String name) {
+        final String[] result = new String[1];
+        result[0]="error";
+        map.forEach((e, v) -> {
+           if (e.equalsIgnoreCase(name)){
+               result[0] =v[0];
+           }
+        });
+        return result[0];
+    }
+
     private void lobbyFunctions(HttpServletRequest request, HttpServletResponse response) {
         try {
 
+            Map<String, String[]> map = request.getParameterMap();
 
-            String mode = request.getParameter("mode");
+            printNames(map);
 
-            String username = request.getParameter("username");
+            String mode = getParmeterValue(map,"mode");//request.getParameter("mode");
+
+            String username = getParmeterValue(map,"username");//request.getParameter("username");
+
 
             switch (mode) {
                 case "join":
@@ -78,7 +106,7 @@ public class LobbyServlet extends HttpServlet {
                     //TODO:Return Erorr
                     break;
             }
-        }catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
