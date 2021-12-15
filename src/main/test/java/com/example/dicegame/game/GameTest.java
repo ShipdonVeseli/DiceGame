2d3d49d43afc422e9b8795bd4da184a4879eac1d
LAPTOP-JAEFD5TU\lucas <lucaswizard98@gmail.com>
2021-12-15 15:36:00 +0100

    +test

---
+++ src/main/test/java/com/example/dicegame/game/GameTest.java
@@ -0,0 +1,19 @@
+package com.example.dicegame.game;
+
+import com.example.dicegame.random.DiceManager;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @BeforeEach
+    void setUp() {
+        DiceManager diceManager=DiceManager.getInstanz();
+    }
+
+
+
+
+
+}
