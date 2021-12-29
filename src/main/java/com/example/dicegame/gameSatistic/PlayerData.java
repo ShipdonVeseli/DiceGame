4f5d58ed5c8c9421d9917fbc6aa4e2bc97ae6b19
LAPTOP-JAEFD5TU\lucas <lucaswizard98@gmail.com>
2021-12-29 16:22:27 +0100

    + player Data

---
+++ src/main/java/com/example/dicegame/gameSatistic/PlayerData.java
@@ -0,0 +1,10 @@
+package com.example.dicegame.gameSatistic;
+
+import java.util.ArrayList;
+
+public class PlayerData {
+    private String PlayerName;
+    private ArrayList<Integer> diceValues=new ArrayList<>();
+    private ArrayList<Integer> movedResources=new ArrayList<>();
+
+}
