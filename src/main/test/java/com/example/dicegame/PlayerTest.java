1b1438b122698a7b6f02a594570196f4f655b3c2
LAPTOP-JAEFD5TU\lucas <lucaswizard98@gmail.com>
2022-03-01 14:04:21 +0100

    implented ai

--- src/main/test/java/com/example/dicegame/PlayerTest.java
+++ src/main/test/java/com/example/dicegame/PlayerTest.java
@@ -1,10 +1,20 @@
 package com.example.dicegame;
 
+import com.example.dicegame.game.Resource;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
+
+    @Test
+    void testEquals(){
+        Player player=new Player("seef");
+        player.addResource(new Resource());
+
+        assertTrue(player.equals(player));
+
+    }
 
     @Test
     void convertToJSONTest() {
