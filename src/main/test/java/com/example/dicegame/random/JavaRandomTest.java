68f5cb6069f5a00aff55852b066a9d24a9e8de85
LAPTOP-JAEFD5TU\lucas <lucaswizard98@gmail.com>
2022-06-21 18:27:58 +0200

    +tests

---
+++ src/main/test/java/com/example/dicegame/random/JavaRandomTest.java
@@ -0,0 +1,15 @@
+package com.example.dicegame.random;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JavaRandomTest {
+
+    @Test
+    void fillQueueTest(){
+        //ToDo
+    }
+
+
+}
