00a7a98eb973d19a176d9201aecee9aa71af630b
LAPTOP-JAEFD5TU\lucas <lucaswizard98@gmail.com>
2022-06-21 18:17:14 +0200

    +tests

--- src/main/test/java/com/example/dicegame/random/DiceManagerTest.java
+++ src/main/test/java/com/example/dicegame/random/DiceManagerTest.java
@@ -8,47 +8,95 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DiceManagerTest {
 
-    DiceManager diceManager=DiceManager.getInstanz();
+    DiceManager diceManager;
+
     @BeforeEach
     void setUp() {
+        diceManager = DiceManager.getInstanz();
     }
 
     @RepeatedTest(100)
-    public void testGetDice(){
+    public void testGetDice() {
         try {
 
-            int result=diceManager.getDice(1,6);
+            int result = diceManager.getDice(1, 6);
 
-            System.out.println("result= "+result);
-            assertTrue(result>=1&&result<=6);
+            System.out.println("result= " + result);
+            assertTrue(result >= 1 && result <= 6);
 
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             fail();
         }
 
     }
 
+
     @Test
-    public void testGetDiceMinMac(){
+    public void testGetDice2() {
         try {
 
-            int result1=diceManager.getDice(1,6);
-            int result2=diceManager.getDice(2,8);
-            int result3=diceManager.getDice(2,8);
+            int result = diceManager.getDice(1, 1);
 
-            System.out.println("result= "+result1);
-            System.out.println("result2= "+result2);
-            System.out.println("result3= "+result3);
+            System.out.println("result= " + result);
+            assertTrue(result >= 1 && result <= 6);
 
-            int size=diceManager.getRandomSets().size();
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail();
+        }
 
-            assertTrue(result1>=1&&result1<=6);
-            assertTrue(result2>=2&&result2<=8);
-            assertTrue(result3>=1&&result3<=8);
-            assertEquals(2,size);
+    }
 
-        }catch (Exception e){
+
+    @Test
+    public void testGetSize() {
+        try {
+            int expectedResult = 2;
+            int size;
+
+            diceManager.getDice(1, 6);
+            diceManager.getDice(1, 6);
+            diceManager.getDice(2, 8);
+            diceManager.getDice(2, 8);
+
+            size = diceManager.getRandomSets().size();
+
+            assertEquals(expectedResult, size);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+
+    @Test
+    public void testGetSize2() {
+        try {
+            int size = diceManager.getRandomSets().size();
+            int expectedResult = 0;
+
+
+            assertEquals(expectedResult, size);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+    @Test
+    public void testGetSize3() {
+        try {
+
+            int size = diceManager.getRandomSets().size();
+            int expectedResult = 1;
+
+            diceManager.getDice(1, 6);
+
+            assertEquals(expectedResult, size);
+        } catch (Exception e) {
             e.printStackTrace();
             fail();
         }
