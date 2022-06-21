084e51c95c7da2bd5e27a0ae5038a597f4f368c6
LAPTOP-JAEFD5TU\lucas <lucaswizard98@gmail.com>
2022-06-21 18:31:57 +0200

    +tests

---
+++ src/main/test/java/com/example/dicegame/game/VoteTest.java
@@ -0,0 +1,7 @@
+package com.example.dicegame.game;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VoteTest {
+
+}
---
+++ src/main/test/java/com/example/dicegame/gameSatistic/RessourceDataTest.java
@@ -0,0 +1,7 @@
+package com.example.dicegame.gameSatistic;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RessourceDataTest {
+
+}
